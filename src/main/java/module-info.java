52baa54e6a571165d7a37826f94b6d917a7df3be
tien-th <tien.nh205033@sis.hust.edu.vn>
2023-06-13 20:31:41 +0700

    add readme

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module com.example.itssn11 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.sql;
+
+
     opens com.example.itssn11 to javafx.fxml;
     exports com.example.itssn11;
+
+    opens dangnhap to javafx.praphics, javafx.fxml;
+    exports dangnhap;
 }
