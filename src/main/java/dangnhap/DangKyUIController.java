7fdc9e8d34902ec57920b60469685ac9bc5a7d85
tien-th <tien.nh205033@sis.hust.edu.vn>
2023-06-16 17:31:36 +0700

    add admin's menu interface

--- src/main/java/dangnhap/DangKyUIController.java
+++ src/main/java/dangnhap/DangKyUIController.java
@@ -7,7 +7,6 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-
 import java.sql.SQLException;
 
 public class DangKyUIController {
---
+++ src/main/java/menu/AdminUIController.java
@@ -0,0 +1,28 @@
+package menu;
+
+import javafx.event.ActionEvent;
+
+public class AdminUIController {
+
+
+    public void viewUserUI(ActionEvent actionEvent) {
+    }
+
+    public void viewPetUI(ActionEvent actionEvent) {
+    }
+
+    public void viewManageAppointmentsUI(ActionEvent actionEvent) {
+        
+    }
+
+    public void viewManageCleaningUI(ActionEvent actionEvent) {
+    }
+
+    public void viewManageMedicineUI(ActionEvent actionEvent) {
+    }
+    public void viewManageCustodialUI(ActionEvent actionEvent) {
+    }
+
+    public void viewStatisticalUI(ActionEvent actionEvent) {
+    }
+}
